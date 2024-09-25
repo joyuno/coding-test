3626549804f178c3ab771f534127aba59e3ee285
joyuno <90371163+joyuno@users.noreply.github.com>
2024-09-25 09:59:14 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,27 @@
+a,b,c,d,e,f=map(int,input().split())
+if a!=1:
+    a=1-a
+else:
+    a=0
+if b!=1:
+    b=1-b
+else:
+    b=0
+if c!=2:
+    c=2-c
+else:
+    c=0
+if d!=2:
+    d=2-d
+else:
+    d=0
+if e!=2:
+    e=2-e
+else:
+    e=0
+if f!=8:
+    f=8-f
+else: 
+    f=0
+    
+print(a,b,c,d,e,f)
